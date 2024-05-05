3b4990222b5e3af368eb4fb85334c5e8acf7c6ea
Fabio Gomez <fabiogomez11c@gmail.com>
2024-05-05 08:26:30 -0500

    not finished yet

--- poc.py
+++ poc.py
@@ -2,7 +2,7 @@ from datasets.arrow_dataset import Dataset
 from datasets import load_dataset
 
 import dspy
-from dspy.teleprompt import BootstrapFewShotWithRandomSearch
+from dspy.teleprompt import BootstrapFewShotWithRandomSearch, MIPRO
 from dspy.evaluate import Evaluate
 
 from utils import examples_from_dataset
@@ -15,7 +15,7 @@ logging.basicConfig(
     filename='app.log',
     filemode='w', format='%(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
-    )
+)
 
 
 # Dataset handling/importing
@@ -31,7 +31,8 @@ train_examples = examples_from_dataset(dataset['train'])
 test_examples = examples_from_dataset(dataset['test'])
 
 # model = dspy.OllamaLocal(model='llama3')
-model = dspy.OpenAI(model='gpt-3.5-turbo-0125', max_tokens=1000)
+model = dspy.OllamaLocal(model='phi3')
+# model = dspy.OpenAI(model='gpt-3.5-turbo-0125', max_tokens=1000)
 dspy.settings.configure(lm=model)
 
 basic_prompt = dspy.ChainOfThought("question -> answer")
@@ -53,7 +54,7 @@ evaluate = Evaluate(
     metric=exact_match,
     display_progress=True,
     num_threads=4
-    )
+)
 
 # evalute without optim
 print("EVAL W/O OPTIMIZATION +++++++++++++++++++++")
@@ -63,21 +64,37 @@ print("COT with context")
 evaluate(cot_with_context)
 
 # optimization
-config = dict(
+config_bootstrap = dict(
     max_bootstrapped_demos=8,
     max_labeled_demos=8,
     num_candidate_programs=10
 )
-teleprompter = BootstrapFewShotWithRandomSearch(
+# teleprompter = BootstrapFewShotWithRandomSearch(
+#     metric=exact_match,
+#     **config_bootstrap
+# )
+config_mipro = dict(
+    num_candidates=5,
+    init_temperature=1.0,
+)
+teleprompter = MIPRO(
     metric=exact_match,
-    **config
+    **config_mipro
 )
 print("OPTIMIZATION +++++++++++++++++++++")
 print("COT with context")
+# opt_cot_with_context = teleprompter.compile(
+#     cot_with_context,
+#     trainset=train_examples,
+# )
 opt_cot_with_context = teleprompter.compile(
     cot_with_context,
-    trainset=train_examples
-    )
+    trainset=train_examples,
+    num_trials=5,
+    max_bootstrapped_demos=8,
+    max_labeled_demos=8,
+    eval_kwargs={'num_threads': 4}
+)
 
 # evalute after optim
 print("EVAL AFTER OPTIMIZATION +++++++++++++++++++++")
